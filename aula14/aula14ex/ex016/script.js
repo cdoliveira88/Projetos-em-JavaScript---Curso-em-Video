3d349b6db9ffe5f639cc3ed1c46d2bac1b2cf0ba
Celso Dias <cdoliveira_88@hotmail.com>
2020-08-05 19:03:36 -0300

    Continuação dos exercícios
    Fazendo a tabuada

--- aula14/aula14ex/ex016/script.js
+++ aula14/aula14ex/ex016/script.js
@@ -1,15 +1,33 @@
 function contar() {
-    document.querySelector('div#res', 'div#mostrar')
-    var num = document.querySelector('input#inicio')
-    var final = document.querySelector('input#fim')
-    var pass = document.querySelector('input#passo')
-    res.innerHTML = 'Contando:'
-    if (final.value == 0) {
-        alert('Revise os dados informados!')
-    }
-    for (num.value; num.value <= final.value; num.value += pass.value) {
-        mostrar.innerHTML = `${num.value}`
+    var num = document.querySelector('input#txti')
+    var final = document.querySelector('input#txtf')
+    var pass = document.querySelector('input#txtp')
+    var resp = document.getElementById('res')
 
-    }
 
+    if (num.value.length == 0 || final.value.length == 0 || pass.value.length == 0) {
+        window.alert('Revise os dados informados!')
+    } else {
+        res.innerHTML = 'Contando... <br>'
+        var ini = Number(num.value)
+        var fin = Number(final.value)
+        var pas = Number(pass.value)
+        if (pas == 0) {
+            window.alert('Passo inválido. Considerando passo 1')
+            pas = 1
+        }
+
+        if (ini < fin) {
+            //contagenm crescente
+            for (var c = ini; c <= fin; c += pas) {
+                resp.innerHTML += `${c} \u{1F603}` //formato do código para colocar emoction, só funciona entre crazes
+            }
+        } else {
+            //contagem regressiva
+            for (var c = ini; c >= fin; c -= pas) {
+                res.innerHTML += `${c} \u{1F603}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
--- aula14/aula14ex/ex017/script.js
+++ aula14/aula14ex/ex017/script.js
@@ -1,1 +1,15 @@
-alert('Olá!')
+function calcular() {
+    var num = document.querySelector('input#txtn')
+    var resp = document.getElementById('txtf')
+    if (num.value.length == 0) {
+        window.alert('Verifique o valor')
+    } else {
+        txtf.innerHTML = 'Contando...'
+        var n = Number(num.value)
+        for (var c = 1; c <= 10; c++) {
+            n *= c
+            txtf.innerHTML = `${c}x${}=`
+        }
+    }
+
+}
