11f86752be204bad9ac27449f3ee3ac6800fece4
Celso Dias <cdoliveira_88@hotmail.com>
2020-08-07 19:21:27 -0300

    continuação dos exercícios
    aula 14

--- aula14/aula14ex/ex017/script.js
+++ aula14/aula14ex/ex017/script.js
@@ -1,15 +1,20 @@
 function calcular() {
     var num = document.querySelector('input#txtn')
-    var resp = document.getElementById('txtf')
+    var tab = document.getElementById('seltab')
     if (num.value.length == 0) {
-        window.alert('Verifique o valor')
+        window.alert('Por favor, digite um número!')
     } else {
-        txtf.innerHTML = 'Contando...'
+        //txtf.innerHTML = 'Contando...'
         var n = Number(num.value)
+            //var c = 1
+        tab.innerHTML = ''
         for (var c = 1; c <= 10; c++) {
-            n *= c
-            txtf.innerHTML = `${c}x${}=`
+            let item = document.createElement('option') //criando um elemento dentro da select
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //esse value no item faz mais sentido no PHP, para que cada item tenha um valor
+            tab.appendChild(item)
+                //c++
         }
-    }
 
+    }
 }
